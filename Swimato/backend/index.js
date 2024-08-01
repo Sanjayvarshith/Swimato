d847bb47b49251822fd59e4a512e6d0efab3c1e6
Sanjayvarshith <133268176+Sanjayvarshith@users.noreply.github.com>
2024-08-01 22:34:14 +0530

    Add files via upload

---
+++ Swimato/backend/index.js
@@ -0,0 +1,37 @@
+
+const express = require('express')
+const app = express()
+const port = 5000
+const mongoDB=require("./db")
+mongoDB();
+app.use(express.json())
+
+// app.use((req,res,next)=>{
+//   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-All-Headers"
+//   )
+//   res.header(
+//     "Access-Control-Allow-All-Methods"
+//   )
+// })
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use(express.json())
+app.use('/api',require("./Routes/CreateUser"));
+app.use('/api',require("./Routes/DisplayData"));
+app.use('/api',require("./Routes/OrderData"));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
